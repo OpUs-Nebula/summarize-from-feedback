fa3c43dc88c955efaba296c8501585f1da5e0124
Mbwenga Maliti <mbwenga.maliti@gmail.com>
2021-03-31 15:57:04 +0200

    Switching hyperparameter struct for local exp

--- exps/local_sample.py
+++ exps/local_sample.py
@@ -1,11 +1,15 @@
 from summarize_from_feedback import sample
 from summarize_from_feedback.utils import experiment_helpers as utils
+from summarize_from_feedback.utils import query_response_model as run
 
-HParams = sample.HParams(
-	model_spec = utils.sup4_ppo_rm4(),
-	task = utils.tldr_task,
-    query_dataset_split= "valid",
-    num_queries = 4
-	)
+
+# HParams = sample.HParams(
+# 	model_spec = utils.sup4_ppo_rm4(),
+# 	task = utils.tldr_task,
+#     query_dataset_split= "valid",
+#     num_queries = 4
+# 	)
+
+HParams = run.RunParams()
 
 sample.main(H = HParams)
